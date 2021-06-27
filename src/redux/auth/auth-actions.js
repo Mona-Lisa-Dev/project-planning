e3562c836bebaf340045a8450c5a329faf365aed
Lena <elena.hlihan@gmail.com>
2021-06-28 02:50:57 +0300

    auth-actions

---
+++ src/redux/auth/auth-actions.js
@@ -0,0 +1,15 @@
+import { createAction } from '@reduxjs/toolkit';
+
+export const signupRequest = createAction('auth/signupRequest');
+export const signupSuccess = createAction('auth/signupSuccess');
+export const signupError = createAction('auth/signupError');
+
+export const loginRequest = createAction('auth/loginRequest');
+export const loginSuccess = createAction('auth/loginSuccess');
+export const loginError = createAction('auth/loginError');
+
+export const logoutRequest = createAction('auth/logoutRequest');
+export const logoutSuccess = createAction('auth/logoutSuccess');
+export const logoutError = createAction('auth/logoutError');
+
+// export const clearError = createAction('auth/clearError');
