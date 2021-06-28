c10dcc616a2237eaca2d1a47eb073c83bbd54fc8
Vadim-Vatrya <VatryaVV@gmail.com>
2021-06-28 22:04:04 +0300

    add state and handleChange

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import styles from './LoginPage.module.scss';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = event => {
+    const { name, value } = event.currentTarget;
+
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+
+      case 'password':
+        setPassword(value);
+        break;
+
+      default:
+        return;
+    }
+  };
+
   return <h1 className={styles.title}>Login Page</h1>;
 };
 
