b18138bd58df0e7bbc89fb771dcf484b1f88547f
Lena <elena.hlihan@gmail.com>
2021-06-28 17:18:38 +0300

    add plug

---
+++ src/redux/projects/projects-operations.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const getAllProjects = () => async dispatch => {
+  try {
+  } catch (error) {}
+};
+
+const createProject = () => async dispatch => {
+  try {
+  } catch (error) {}
+};
+
+const deleteProject = () => async dispatch => {
+  try {
+  } catch (error) {}
+};
+
+const updateProjectName = () => async dispatch => {
+  try {
+  } catch (error) {}
+};
+
+const addParticipant = () => async dispatch => {
+  try {
+  } catch (error) {}
+};
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  getAllProjects,
+  createProject,
+  deleteProject,
+  updateProjectName,
+  addParticipant,
+};
