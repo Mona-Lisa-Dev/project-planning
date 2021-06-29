12d63db90d19a9fb760a0ae1942b46474954f0a0
Nataliia333 <n.o.prykhodko@gmail.com>
2021-06-29 03:06:40 +0300

    propTypes for sprintItem

--- src/components/SprintItem/SprintItem.jsx
+++ src/components/SprintItem/SprintItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './SprintItem.module.scss';
+import PropTypes from 'prop-types';
 // import ButtonDelete from '../ButtonDelete';
 
 const SprintItem = ({ name, startDate, endDate, duration }) => {
   return (
     <>
       <div className={styles.item_one}>
-        <h3 className={styles.item_header}>Name{name}</h3>
+        <h3 className={styles.item_header}>{name}</h3>
         <div className={styles.item_info}>
           <div className={styles.item_left}>
             <p className={styles.item_description}>Дата початку</p>
@@ -14,9 +15,9 @@ const SprintItem = ({ name, startDate, endDate, duration }) => {
             <p className={styles.item_description}>Тривалість</p>
           </div>
           <div className={styles.item_right}>
-            <p className={styles.item_description}>1{startDate}</p>
-            <p className={styles.item_description}>1{endDate}</p>
-            <p className={styles.item_description}>1{duration}</p>
+            <p className={styles.item_description}>{startDate}</p>
+            <p className={styles.item_description}>{endDate}</p>
+            <p className={styles.item_description}>{duration}</p>
           </div>
         </div>
         {/* <ButtonDelete /> */}
@@ -26,3 +27,10 @@ const SprintItem = ({ name, startDate, endDate, duration }) => {
 };
 
 export default SprintItem;
+
+SprintItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  startDate: PropTypes.string.isRequired,
+  endDate: PropTypes.string.isRequired,
+  duration: PropTypes.number.isRequired,
+};
