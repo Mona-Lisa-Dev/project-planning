14e90bf6aabb9c9077853227a6323009cabe4135
Lena <elena.hlihan@gmail.com>
2021-06-30 04:54:43 +0300

    add old version login/register page

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,34 +1,14 @@
 import { useState } from 'react';
-
-
-import { Formik } from 'formik';
-import * as yup from 'yup';
-
-
 import { useDispatch } from 'react-redux';
 
-import styles from './RegisterPage.module.scss';
-
 import authOperations from 'redux/auth/auth-operations';
+import styles from './RegisterPage.module.scss';
 
 const RegisterPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const validationSchema = yup.object().shape({
-    email: yup.string().email('Type correct email').required('Required'),
-    password: yup
-      .string()
-      .typeError('Must be a string')
-      .min(6, 'Wrong password')
-      // .matches('/^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,}$/', 'Wrong password')
-      .required('Required'),
-    confirmPassword: yup
-      .string()
-      .oneOf([yup.ref('password')], 'Passwords do not match')
-      .required('Required'),
-  });
-
+  const dispatch = useDispatch();
 
   const handleChange = event => {
     const { name, value } = event.currentTarget;
@@ -47,16 +27,11 @@ const RegisterPage = () => {
     }
   };
 
-
   const handleFormSubmit = event => {
     event.preventDefault();
 
-    // const user = { email, password };
-    // dispatch(authOperations.signUp(user));
-
     const user = { email, password };
     dispatch(authOperations.signup(user));
-
 
     reset();
   };
@@ -67,112 +42,74 @@ const RegisterPage = () => {
   };
 
   return (
-    <div>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validateOnBlur
-        onSubmit={(values, { reset }) => {
-          handleFormSubmit({ email: values.email, password: values.password });
-          reset();
-        }}
-        validationSchema={validationSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          isValid,
-          handleSubmit,
-          dirty,
-        }) => (
-          <div className={styles.backgroundMain}>
-            <div className={styles.registration}>
-              <div className={`${styles.general} ${styles.ellipse1}`}></div>
-              <div className={`${styles.general} ${styles.ellipse2}`}></div>
-              <div className={`${styles.general} ${styles.ellipse3}`}></div>
-              <div className={`${styles.general} ${styles.ellipse4}`}></div>
-              <div className={`${styles.general} ${styles.ellipse5}`}></div>
-              <div className={`${styles.general} ${styles.ellipse6}`}></div>
-              <div className={`${styles.general} ${styles.ellipse7}`}></div>
-              <div className={`${styles.general} ${styles.ellipse8}`}></div>
-              <div className={`${styles.general} ${styles.ellipse9}`}></div>
-              <div className={`${styles.general} ${styles.ellipse10}`}></div>
-            </div>
-            <form
-              onSubmit={handleFormSubmit}
-              className={styles.form}
-              autoComplete="off"
-            >
-              <h1 className={styles.title}>Enter</h1>
-              <label className={styles.labelForm}>
-                <input
-                  onBlur={handleBlur}
-                  className={styles.inputForm}
-                  placeholder=" "
-                  type={'email'}
-                  name={'email'}
-                  onChange={handleChange}
-                  value={values.email}
-                />
-                <span className={styles.nameInput}>E-mail</span>
-                {touched.email && errors.email && (
-                  <p className={styles.error}>{errors.email}</p>
-                )}
-              </label>
+    <div className={styles.backgroundMain}>
+      <div className={styles.registration}>
+        <div className={`${styles.general} ${styles.ellipse1}`}></div>
+        <div className={`${styles.general} ${styles.ellipse2}`}></div>
+        <div className={`${styles.general} ${styles.ellipse3}`}></div>
+        <div className={`${styles.general} ${styles.ellipse4}`}></div>
+        <div className={`${styles.general} ${styles.ellipse5}`}></div>
+        <div className={`${styles.general} ${styles.ellipse6}`}></div>
+        <div className={`${styles.general} ${styles.ellipse7}`}></div>
+        <div className={`${styles.general} ${styles.ellipse8}`}></div>
+        <div className={`${styles.general} ${styles.ellipse9}`}></div>
+        <div className={`${styles.general} ${styles.ellipse10}`}></div>
+      </div>
 
-              <label className={styles.labelForm}>
-                <input
-                  onBlur={handleBlur}
-                  className={styles.inputForm}
-                  placeholder=" "
-                  type={'password'}
-                  name={'password'}
-                  onChange={handleChange}
-                  value={values.password}
-                />
-                <span className={styles.nameInput}>Password</span>
-                {touched.password && errors.password && (
-                  <p className={styles.error}>{errors.password}</p>
-                )}
-              </label>
-              <label className={styles.labelForm}>
-                <input
-                  onBlur={handleBlur}
-                  className={styles.inputForm}
-                  placeholder=" "
-                  type={'password'}
-                  name={'confirmPassword'}
-                  onChange={handleChange}
-                  value={values.confirmPassword}
-                />
-                <span className={styles.nameInput}>Password</span>
-                {touched.confirmPassword && errors.confirmPassword && (
-                  <p className={styles.error}>{errors.confirmPassword}</p>
-                )}
-              </label>
-              <button
-                disabled={!isValid || !dirty}
-                onClick={handleSubmit}
-                className={styles.btnLog}
-                type={'submit'}
-              >
-                Enter
-              </button>
-              <div className={styles.login}>
-                <p className={styles.question}> No account? </p>
-                <a className={styles.auth} href="/users/signup">
-                  Register
-                </a>
-              </div>
-            </form>
-          </div>
-        )}
-      </Formik>
+      <form
+        onSubmit={handleFormSubmit}
+        className={styles.form}
+        autoComplete="off"
+      >
+        <h1 className={styles.title}>Registration</h1>
+        <label className={styles.labelForm}>
+          <input
+            className={styles.inputForm}
+            placeholder=" "
+            type={'email'}
+            name={'email'}
+            onChange={handleChange}
+            value={email}
+          />
+          <span className={styles.nameInput}>E-mail</span>
+        </label>
+
+        <label className={styles.labelForm}>
+          <input
+            className={styles.inputForm}
+            placeholder=" "
+            type={'password'}
+            name={'password'}
+            onChange={handleChange}
+            value={password}
+          />
+          <span className={styles.nameInput}>Password</span>
+        </label>
+
+        <label className={styles.labelForm}>
+          <input
+            className={styles.inputForm}
+            placeholder=" "
+            type={'password'}
+            name={'confirmPassword'}
+            onChange={handleChange}
+            pattern="^[a-z0-9_-]{7,18}$"
+            // value={confirmPassword}
+          />
+          <span className={styles.nameInput}>Repeat password</span>
+        </label>
+
+        <button className={styles.btnReg} type={'submit'}>
+          Register
+        </button>
+
+        <div className={styles.login}>
+          <p className={styles.question}> Do you have an account?</p>
+          <a className={styles.auth} href="/login">
+            Log in
+          </a>
+        </div>
+      </form>
     </div>
   );
 };
