7f62fba2714a903bedda353a7902dc03f81545a9
Lena <elena.hlihan@gmail.com>
2021-07-01 04:47:40 +0300

    add CurrentProject in redux

--- src/redux/projects/projects-selectors.js
+++ src/redux/projects/projects-selectors.js
@@ -1,3 +1,4 @@
 export const getProjects = state => state.projects.items;
+export const getCurrentProject = state => state.projects.currentProject;
 export const getLoadingProjects = state => state.projects.loading;
 export const getError = state => state.projects.error;
