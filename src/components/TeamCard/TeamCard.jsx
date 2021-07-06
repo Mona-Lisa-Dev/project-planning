cd9dc1890651399901b090940e3e770986960a3e
OlgaTomilko <67008325+OlgaTomilko@users.noreply.github.com>
2021-07-06 14:56:05 +0300

    Добавила фото

--- src/components/TeamCard/TeamCard.jsx
+++ src/components/TeamCard/TeamCard.jsx
@@ -1,9 +1,10 @@
 import styles from './TeamCard.module.scss';
 
 const TeamCard = ({ team }) => {
+  const photo = require(`./${team.photo}`).default;
   return (
     <div className={styles.card}>
-      <img src="" alt="" className={styles.photo} />
+      <img src={photo} alt="teammate" className={styles.photo} />
       <h3 className={styles.name}>{team.name}</h3>
       <p className={styles.doing}> {team.doing}</p>
       <a href={team.linkedin} className={styles.links}>
