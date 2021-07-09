c0e6d225b4d17d8b46b4d7a8ae3ae7d5257691c2
Ihor Prokopets <kiringa23@gmail.com>
2021-07-09 12:49:24 +0300

    Change button text

--- src/components/SprintItem/SprintItem.jsx
+++ src/components/SprintItem/SprintItem.jsx
@@ -37,7 +37,7 @@ const SprintItem = ({ currentProject, sprint }) => {
                 onClose();
               }}
             >
-              Ready
+              Ok
             </button>
           </div>
         );
